2437235700cc3189c9e34206cdef0c1f65ccf7d0
Manuel Alejandro Perez Benitez <72420604+SoyManuelPerez@users.noreply.github.com>
2026-09-24 13:03:59 -0500

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'app-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/static/css/styles.css',
+  '/static/js/main.js',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
